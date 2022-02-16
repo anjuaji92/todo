2dc7b474a206d8be0254625c9cf7393d11e18ce1
anjuaji92 <97083740+anjuaji92@users.noreply.github.com>
2022-02-16 23:38:01 +0530

    new changes

--- ajax.js
+++ ajax.js
@@ -1,15 +1,16 @@
 function ajax()
 {
-    var xhttp=new.XMLHttpRequest();
-    xhttp.onreadystatechang=function()
+    var xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange=function()
     {
         if(this.readyState==4&&this.status==200)
         {
-            var response=JSON.parse(this.response);
+            var response=JSON.parse(this.responseText);
+            console.log(response);
             var output="";
             for(var i=0;i<response.length;i++)
             {
-                output+="<li>"+respo[i].title+"</li>";
+                output +="<li>"+response[i].title +"</li>";
             }
             document.getElementById("demo").innerHTML=output;
         }
