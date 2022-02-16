aa7ce7cbe19e0f6307022240f417babdc196a136
anjuaji92 <97083740+anjuaji92@users.noreply.github.com>
2022-02-16 17:49:39 +0530

    new changes

---
+++ login.js
@@ -0,0 +1,24 @@
+var usr=document.getElementById("usr");
+var pwd=document.getElementById("pwd");
+function validate()
+{
+    if(usr.value==""||pwd.value=="")
+    {
+        alert("Fields can't be empty");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function validate2()
+{
+    if(usr.value=="admin" && pwd.value=="12345")
+    {
+        return true;
+    }
+    else{
+        alert("Either usr or pwd is invalid");
+        return false;
+    }
+}
